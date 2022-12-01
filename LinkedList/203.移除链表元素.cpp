83690288fa2d2164c570c00b2c1db07ff07df609
focuseds <h3ureux@gmail.com>
2022-12-01 13:13:17 +0800

    Update 203 [method: virtual head] [35.26 44.6]

--- LinkedList/203.移除链表元素.cpp
+++ LinkedList/203.移除链表元素.cpp
@@ -15,17 +15,39 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+// 头节点和其他节点分开处理
+// class Solution {
+// public:
+//     ListNode* removeElements(ListNode* head, int val) {
+//         // 删除头结点
+//         while (head != nullptr && head->val == val) {
+//             ListNode * tmp = head;
+//             head = tmp->next;
+//             delete tmp;
+//         }
+//         // 删除非头结点
+//         ListNode * cur = head;
+//         while (cur != nullptr && cur->next != nullptr) {
+//             if (cur->next->val == val) {
+//                 ListNode * tmp = cur->next;
+//                 cur->next = tmp->next;
+//                 delete tmp;
+//             } else {
+//                 cur = cur->next;
+//             }
+//         }
+//         return head;
+//     }
+// };
+
+// 设置虚拟头节点
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        // 删除头结点
-        while (head != nullptr && head->val == val) {
-            ListNode * tmp = head;
-            head = tmp->next;
-            delete tmp;
-        }
-        // 删除非头结点
-        ListNode * cur = head;
+        ListNode * pos = new ListNode();
+        pos->next = head;
+        ListNode * cur = pos;
         while (cur != nullptr && cur->next != nullptr) {
             if (cur->next->val == val) {
                 ListNode * tmp = cur->next;
@@ -35,7 +57,7 @@ public:
                 cur = cur->next;
             }
         }
-        return head;
+        return pos->next;
     }
 };
 // @lc code=end
