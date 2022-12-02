33541c02ddd710e6c5e5453fe336ed22a38856de
focuseds <h3ureux@gmail.com>
2022-12-02 16:31:32 +0800

    Solve 206 with [method: recursion] [4.95 96.08]

--- LinkedList/206.反转链表.cpp
+++ LinkedList/206.反转链表.cpp
@@ -16,21 +16,39 @@
  * };
  */
 // double ptr
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         ListNode * pre = nullptr;
+//         ListNode * cur = head;
+//         // ListNode * next = cur->next 会越界操作空指针
+//         ListNode * next;
+
+//         while (cur) {
+//             next = cur->next;
+//             cur->next = pre;
+//             pre = cur;
+//             cur = next;
+//         }
+//         return pre;
+//     }
+// };
+
+// recursion
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-        ListNode * pre = nullptr;
-        ListNode * cur = head;
-        // ListNode * next = cur->next 会越界操作空指针
+    ListNode * reverse(ListNode * pre, ListNode * cur) {
+        // 终止条件
+        if (cur == nullptr) return pre;
+        // 反转操作
         ListNode * next;
-
-        while (cur) {
-            next = cur->next;
-            cur->next = pre;
-            pre = cur;
-            cur = next;
-        }
-        return pre;
+        next = cur->next;
+        cur->next = pre;
+        // 反转下一对pre和cur
+        return reverse(cur, next);
+    }
+    ListNode* reverseList(ListNode* head) {
+        return reverse(nullptr, head);
     }
 };
 // @lc code=end
