020d96704cb215efdf690930757cd210272bfd53
focuseds <h3ureux@gmail.com>
2022-12-11 14:49:35 +0800

    Solve 454 with [method: hash] [18.37 54.74]

--- HashTable/454.四数相加-ii.cpp
+++ HashTable/454.四数相加-ii.cpp
@@ -7,25 +7,46 @@
 // @lc code=start
 
 // violence - time limit exceeded
+// class Solution {
+// public:
+//     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+//         int count = 0;
+//         for (int a : nums1) {
+//             for (int b : nums2) {
+//                 for (int c : nums3) {
+//                     for (int d : nums4) {
+//                         if (a+b+c+d == 0) {
+//                             count = count + 1;
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+// hash
 class Solution {
 public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
         int count = 0;
+
+        unordered_map<int, int> preSum;
         for (int a : nums1) {
-            for (int b : nums2) {
-                for (int c : nums3) {
-                    for (int d : nums4) {
-                        if (a+b+c+d == 0) {
-                            count = count + 1;
-                        }
-                    }
+            for (int b : nums2) preSum[a+b] = preSum[a+b] + 1;
+        }
+
+        for (int c : nums3) {
+            for (int d : nums4) {
+                int diff = 0 - c - d;
+                if (preSum.find(diff) != preSum.end()) {
+                    count = count + preSum[diff];
                 }
             }
         }
         return count;
     }
 };
-
-
 // @lc code=end
 
